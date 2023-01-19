6ccdffef60b3e61aaaa35bcc3acda00f50a77550
Eddie <zxc1435693@gmail.com>
2023-01-19 23:58:32 +0800

    a001

---
+++ src/a001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class a001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        while (scanner.hasNext()) {
+            s = scanner.nextLine();
+            System.out.println("hello, " + s);
+        }
+    }
+}
